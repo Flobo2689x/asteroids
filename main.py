75e2cc72be285826a1fde712c763a77be0cf6aaf
Flobo2689x <ucraft98@gmail.com>
2025-09-06 23:34:00 +0200

    Rotation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -10,6 +11,11 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         screen.fill((0,0,0))
 
@@ -17,8 +23,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+
+        #GO
+        player.update(dt)
+        player.draw(screen)
+
+
         #REFRESH
         pygame.display.flip()
+        #WAIT
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
